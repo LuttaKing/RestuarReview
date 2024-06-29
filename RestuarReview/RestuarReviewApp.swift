6b7ee158b6fff00531117425eef6d28c3242246f
LuttaKing <denilsonwashuma@gmail.com>
2024-06-29 21:21:30 +0300

    Initial Commit

---
+++ RestuarReview/RestuarReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestuarReviewApp.swift
+//  RestuarReview
+//
+//  Created by Denilson Washuma on 29/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct RestuarReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
