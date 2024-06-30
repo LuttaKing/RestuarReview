303b4e1c2935003721835f522cf14c1938a3cbc1
LuttaKing <denilsonwashuma@gmail.com>
2024-06-30 23:41:34 +0300

    setup

--- RestuarReview/RestuarReviewApp.swift
+++ RestuarReview/RestuarReviewApp.swift
@@ -7,8 +7,22 @@
 
 import SwiftUI
 
+import FirebaseCore
+
+//firebase setup https://youtu.be/PxAchicsUYY?list=PL9VJ9OpT-IPSM6dFSwQCIl409gNBsqKTe
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
 @main
 struct RestuarReviewApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
